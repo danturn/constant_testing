c5a7e32c712db7d05ffd364c93d9e2ced768b151
berners <matt.berners-lee@cloudhouse.com>
2016-09-30 16:01:02 +0100

    run specified test only work

--- constant_testing.sh
+++ constant_testing.sh
@@ -7,12 +7,12 @@ run_all() {
 
 mix_test() {
   log_info "Running: mix test $1"
-  output=`mix test $1` 
+  output=`mix test $1`
   result=$?
   echo "$output"
   if [[ $result != 0 ]]; then
     log_failure "OOOPPPPSIE - Somewhat predictably, you've broken something"
-  else 
+  else
     log_success "Congratulations, you haven't broken anything... yet..."
   fi
 }
@@ -36,19 +36,19 @@ log_success() {
 }
 
 test_file_changed() {
-  if [ "$mix_path" == "" ]; then 
+  if [ "$mix_path" == "" ]; then
     log_failure "Cannot find a mix project for: $path$file"
   else
     log_debug "You changed the test file: $path$file so I'm running those tests for you"
-    mix_test "$test_path" 
+    mix_test "$test_path"
   fi
 }
 
 file_changed() {
-  if [ -f $test_path ]; then 
+  if [ -f $test_path ]; then
     log_debug "You changed the file: $path$file so I'll run the tests for that"
     mix_test "$test_path"
-  else 
+  else
     log_failure "Uh-oh, You don't have tests for this do you?"
   fi
 }
@@ -68,17 +68,34 @@ install_tools() {
   fi
 }
 
-watch() {
-  install_tools
-  log_info "I'm going to watch you work... in a creepy way"
-  [[ $1 == "--debug" ]] && debug=1 
+run_specified_test_only() {
+  log_debug "You changed the file: $path$file so I'll run mix test $test_path"
+  mix_test "$test_path"
+}
 
-  inotifywait -m -r -q --exclude '(.swp)' -e close_write ./ |  
-    while read path action file; do 
-      if [[ "$file" == *.exs || "$file" == *.ex ]]; then 
+watch() {
+  first_arg_status $1
+  run_only_specified_test=false
+  if [ $first_arg_set == true ]; then
+    run_only_specified_test=true
+    specified_test_path=$1
+    log_info "You cannot be trusted not to break $specified_test_path"
+    log_info "so I'm going to watch you work and make sure you don't... in a micromanagerial way"
+  else
+    log_info "I'm going to watch you work... in a creepy way"
+  fi
+  install_tools
+  [[ $1 == "--debug" ]] && debug=1
+
+  inotifywait -m -r -q --exclude '(.swp)' -e close_write ./ |
+    while read path action file; do
+      if [[ "$file" == *.exs || "$file" == *.ex ]]; then
         mix_path "$path$file"
         cd $mix_path
-        if [[ "$file" == *_test.exs ]]; then test_file_changed
+        if [[ $run_only_specified_test == true ]]; then
+          test_path=$specified_test_path;
+          run_specified_test_only
+        elif [[ "$file" == *_test.exs ]]; then test_file_changed
         elif [[ "$file" == *.ex ]]; then file_changed; fi
         log_info "I'm watching you..."
         cd - > /dev/null
@@ -86,6 +103,12 @@ watch() {
     done
 }
 
-if [ "$1" != "test" ]; then
-  watch
-fi
+first_arg_status() {
+  first_arg_set=""
+  if [ -z ${1+x} ]; then first_arg_set=false; else first_arg_set=true; fi
+}
+
+first_arg_status $1
+if [ "$1" == "test" ]; then :;
+elif [[ $1 == *"test/"*".exs:"* ]]; then watch $1
+elif [ $first_arg_set == false ]; then watch; fi
