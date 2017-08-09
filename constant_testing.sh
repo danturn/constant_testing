fb4726230d1cbd5b85ec5f577f71dfb9f5f00410
berners <matthewbernerslee@gmail.com>
2017-08-09 12:13:39 +0100

    done

--- constant_testing.sh
+++ constant_testing.sh
@@ -57,8 +57,13 @@ mix_path() {
   [[ $1 =~ (^.*/)((test|lib)/.*$) ]]
   mix_path=${BASH_REMATCH[1]}
   file_path=${BASH_REMATCH[2]}
-  test_path=${file_path/#lib/test}
-  test_path=${test_path/%.ex/_test.exs}
+  if [[ -n $test_one_file_path ]]; then
+    echo "mix path is just for one test"
+    test_path=$test_one_file_path
+  else
+    test_path=${file_path/#lib/test}
+    test_path=${test_path/%.ex/_test.exs}
+  fi
 }
 
 install_tools() {
@@ -80,7 +85,7 @@ watch() {
   inotifywait -m -r -q --exclude '(.swp)' -e close_write ./ |  
     while read path action file; do 
       if [[ "$file" == *.exs || "$file" == *.ex ]]; then 
-        mix_path "$path$file"
+        mix_path "$path$file" $test_one_file_path
         cd $mix_path
         if [[ "$file" == *_test.exs ]]; then test_file_changed
         elif [[ "$file" == *.ex ]]; then file_changed; fi
@@ -90,6 +95,23 @@ watch() {
     done
 }
 
+files_to_test() {
+  [[ $1 =~ (test/)([^_]*)(_test.exs)(.*) ]]
+  if [[ ${BASH_REMATCH[1]} == "test/" && ${BASH_REMATCH[3]} == "_test.exs" && -z ${BASH_REMATCH[4]} ]]; then
+    if [[ ! -f $1 ]]; then
+      log_info "The file '$1' does not exist.\nI can only run tests that exist you tool."
+      exit
+    fi
+    test_one_file_path=$1
+  elif [[ -z $1 ]]; then 
+    true
+  else
+    log_info "I did not understand your arguement '$1'\n\nRun me either\n - with no arguement\n - one arguement in the form of 'test/[your_test_name]_test.exs' (to only run that test on any elixir file change)"
+    exit
+  fi
+}
+
 if [ "$1" != "test" ]; then
+  files_to_test $1
   watch
 fi
