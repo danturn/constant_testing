2c33d83d1c7ed2ca90db1733c54645741213e514
rob <rob@whitehat.org.uk>
2018-03-29 15:13:23 +0100

    adding fixed test running

--- constant_testing.sh
+++ constant_testing.sh
@@ -17,7 +17,7 @@ log_info() {
 }
 
 log_debug() {
-  if [[ $debug ]]; then
+  if [[ $DEBUG ]]; then
     echo -e "\e[33m$1\e[39m"
   fi
 }
@@ -48,6 +48,16 @@ file_changed() {
   fi
 }
 
+any_file_changed() {
+  test_path_without_line=${test_path%:*}
+  if [ -f $test_path_without_line ]; then 
+    log_debug "Running fixed tests: $test_path"
+    mix_test "$test_path"
+  else 
+    log_failure "Uh-oh, that path you gave me doesnt exist ($test_path)"
+  fi
+}
+
 mix_path() {
   [[ $1 =~ (^.*/)((test|lib)/.*$) ]]
   mix_path=${BASH_REMATCH[1]}
@@ -71,11 +81,14 @@ watch() {
   install_tools
   clear_screen
   log_info "I'm going to watch you work... in a creepy way"
-  [[ $1 == "--debug" ]] && debug=1 
 
   inotifywait -m -r -q --exclude '(.swp)' -e close_write ./ |  
-    while read path action file; do 
-      if [[ "$file" == *.exs || "$file" == *.ex ]]; then 
+  while read path action file; do 
+    if [[ "$file" == *.exs || "$file" == *.ex ]]; then 
+      if [ $FIXED_FILE ]; then
+        clear_screen
+        any_file_changed
+      else 
         mix_path "$path$file"
         cd $mix_path
         clear_screen
@@ -84,8 +97,27 @@ watch() {
         log_info "I'm watching you..."
         cd - > /dev/null
       fi
-    done
+    fi
+  done
 }
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+
+  case $key in
+    -d|--debug)
+      DEBUG=true
+      shift # past argument
+      ;;
+    *)    # any option
+      FIXED_FILE=true
+      test_path=("$1") 
+      shift # past argument
+      ;;
+  esac
+done
 
 if [ "$1" != "test" ]; then
   watch
