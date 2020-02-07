ed84e30e79905e8783a587777a6b4128bec838fe
Dan Turner <dan@whitehat.org.uk>
2020-02-07 11:44:45 +0000

    running elm watch at the same time

--- setup_in.sh
+++ setup_in.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  apt-get install fswatch -y
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "MAC"        # Mac OSX
+else
+  echo "OMG, what os is this: $OSTYPE"
+  exit -2;
+fi
+
 if [ -d "$1" ]; then
   echo "symlinking to $1"
   ln -f -s $(pwd)/constant_testing $1/constant_testing
